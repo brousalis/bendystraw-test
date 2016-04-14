f9131286b62f62b5be3e1ebd95aff7643e0f8320
Pete Brousalis <brousapg@gmail.com>
2016-04-14 12:40:35 -0500

    [BUGFIX] correct settings for the app

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,17 @@
 require('bendystraw')({
-  bowerImages: ['bower_components/rolodex/**/*']
+  paths: {
+    scripts: 'app'
+  },
+
+  angular: {
+    enabled: true
+  },
+
+  images: {
+    bower: ['bower_components/rolodex/**/*']
+  },
+
+  scripts: {
+    coffeescript: true
+  }
 })
-
-var gulp = require('gulp');
-var fs = require('fs');
-
-gulp.task('ci', function () {
-  var conf = process.env;
-
-  // Pass in --env staging/production
-  var args = process.argv.slice(3);
-
-  // Because circleci
-  var env = args[1].toUpperCase();
-
-  // Pluck the settings we need (stupid)
-  var settings = {};
-  var variables = ['API'];
-
-  variables.forEach(function (variable) {
-    settings[env] = conf[env + variable]
-  })
-
-  var output = '';
-
-  // Make a string to write to file from object
-  Object.keys(settings).forEach(function (key) {
-    output += key + '=' + settings[key] + '\n';
-  })
-
-  fs.writeFile('.env.' + env.toLowerCase(), output);
-});
